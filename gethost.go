fd1d37246c28049fd0167fcd68fdfdcc7716df1c
Phil Hanna <ph1204@gmail.com>
2023-09-08 06:06:06 -0400

    Refactored

--- gethost.go
+++ gethost.go
@@ -1,99 +1,65 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net"
-	"os"
 	"regexp"
 )
 
-// Show usage information
-
-func showUsage() {
-	fmt.Println("usage: GetHost <ipaddr>|<name>")
-	os.Exit(0)
-}
-
 func main() {
 
-	// Check if there are any arguments, and show usage if not
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	flag.Parse()
 
-	if len(os.Args) == 1 {
-		showUsage()
-	}
-
-	// Check whether any of the arguments are help flags, and show usage
-	// if so
-
-	for _, arg := range os.Args[1:] {
-		if arg == "-h" || arg == "--help" {
-			showUsage()
-		}
-	}
-
-	// Look up information for each argument
-
-	for _, addr := range os.Args[1:] {
-
-		// Check whether the argument is an IP address or a host name
-
-		if matched, _ := regexp.MatchString(`[\d.]+`, addr); matched {
-
-			// Check whether the IP address is well-formed
-
-			if m, _ := regexp.MatchString(`(\d+)\.(\d+)\.(\d+)\.(\d+)`, addr); !m {
-				fmt.Printf("Malformed address: %s\n", addr)
-				os.Exit(1)
-			}
-
-			// Look up the hostname, aliases, and IP addresses
-			// associated with the address
-
-			names, err := net.LookupAddr(addr)
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-			ips, _ := net.LookupIP(addr)
-
-			// Print the hostname, aliases, and IP addresses associated
-			// with the address
-
-			PrintNames(names[0], addr, ips)
-
-		} else {
-
-			fmt.Printf("DEBUG: addr=%q", addr)
-			// Look up the IP addresses associated with the hostname
-
-			ips, err := net.LookupIP(addr)
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-
-			// Look up the hostname, aliases, and IP addresses
-			// associated with the first IP address
-
-			names, err := net.LookupAddr(ips[0].String())
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-
-			// Print the hostname, aliases, and IP addresses associated
-			// with the hostname
-
-			PrintNames(names[0], addr, ips)
-		}
+	// Get the IP address or host name
+	addr := flag.Arg(0)
+	re := regexp.MustCompile(`(\d+).(\d+).(\d+).(\d+)`)
+	if re.MatchString(addr) {
+		handleIPAddress(addr)
+	} else {
+		handleHostName(addr)
 	}
 }
 
-func PrintNames(name string, addr string, ips []net.IP) {
-	fmt.Printf("name       = %s\n", name)
-	aliases, _ := net.LookupCNAME(addr)
-	fmt.Printf("aliases    = %s\n", aliases)
-	for _, ip := range ips {
-		fmt.Printf("addrs      = %s\n", ip.String())
+// Handles an IP address of the form 999.999.999.999
+func handleIPAddress(addr string) {
+
+	// Print IP address
+	fmt.Printf("addr       = %s\n", addr)
+
+	// Look up the hostname, aliases, and IP addresses
+	// associated with the address
+	names, err := net.LookupAddr(addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, name := range names {
+		fmt.Printf("name       = %s\n", name)
+	}
+
+}
+
+// Handles a host name string
+func handleHostName(hostName string) {
+
+	// Print host name
+	fmt.Printf("name       = %s\n", hostName)
+
+	// Print canonical name, if available
+	cname, err := net.LookupCNAME(hostName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("alias      = %s\n", cname)
+
+	// Look up the IP addresses associated with the hostname
+	ips, err := net.LookupIP(hostName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, ipAddress := range ips {
+		fmt.Printf("addr       = %s\n", ipAddress.String())
 	}
 }
